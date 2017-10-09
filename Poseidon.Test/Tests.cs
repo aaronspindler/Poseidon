fbecb6337638ebceba841b9cb1ed4ace12bb9de8
Aaron Spindler <aaron@xnovax.net>
2017-10-09 16:38:02 -0400

    Added testing project

---
+++ Poseidon.Test/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Poseidon.Test
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}

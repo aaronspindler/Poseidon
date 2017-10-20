8698f97b1023dffa81ddac14174891e7cfbfdb44
Aaron Spindler <aaron@xnovax.net>
2017-10-20 22:33:20 +0200

    Working on a settings file

--- Poseidon/Program.cs
+++ Poseidon/Program.cs
@@ -1,22 +1,33 @@
 ﻿using System;
-using System.Data;
 using System.IO;
+using System.Net;
 
 namespace Poseidon
 {
     public class Program
     {
+        private static bool NETWORK = false;
         private static string KEY;
         private static string SIGNATURE;
         private static Kraken kraken;
+        private static string currency;
         
 
         private static void Main()
         {
             Console.Title = "Poseidon";
+            CheckNetworkConnection();
             CheckKeyFile();
             LoadKeys();
+            CheckSettingsFile();
+            LoadSettings();
 
+            if (!NETWORK)
+            {
+                Console.WriteLine("No network connection!");
+                ExitProgram();
+            }
+            
             kraken = new Kraken(KEY, SIGNATURE);
             Console.WriteLine(kraken.GetServerTime().result.rfc1123);
 
@@ -63,6 +74,51 @@ namespace Poseidon
             }
         }
 
+        public static void CheckSettingsFile()
+        {
+            if (!File.Exists("settings.txt"))
+            {
+               CreateDefaultSettingsFile();
+            }
+            else
+            {
+                using (var sr = new StreamReader("settings.txt"))
+                {
+                    var line = sr.ReadLine();
+                    if (line.Substring(line.LastIndexOf('='), (line.Length - line.LastIndexOf('='))) !=
+                        System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString())
+                    {
+                        CreateDefaultSettingsFile();
+                        Console.WriteLine("Looks like you were using a previous version of the settings file.");
+                        Console.WriteLine("Your settings file has been remade");
+                    }
+                }
+            }
+        }
+
+        public static void CreateDefaultSettingsFile()
+        {
+            try
+            {
+                using (var sw = File.CreateText("settings.txt"))
+                {
+                    sw.WriteLine("VERSION=" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+                    sw.WriteLine("CURRENCY=CAD");
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+
+                ExitProgram();
+            }
+        }
+
+        public static void LoadSettings()
+        {
+            
+        }
+
 
         public static void ExitProgram()
         {
@@ -94,6 +150,26 @@ namespace Poseidon
             var dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             dtDateTime = dtDateTime.AddSeconds((double) unix).ToLocalTime();
             return dtDateTime.ToString();
+        }
+
+        /// <summary>
+        ///     Checks for an available internet connection by pinging google.com
+        /// </summary>
+        /// <returns></returns>
+        public static void CheckNetworkConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (Stream stream = client.OpenRead("http://www.google.com"))
+                {
+                    NETWORK = true;
+                }
+            }
+            catch
+            {
+                NETWORK = false;
+            }
         }
 
         public static Kraken GetKraken()

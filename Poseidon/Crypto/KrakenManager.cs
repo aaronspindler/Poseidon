b7e0bee645007d007514efa00a6046b1e02feb10
Aaron Spindler <aaron@xnovax.net>
2019-03-25 20:26:39 -0400

    Working on KrakenManager.cs

--- Poseidon/Crypto/KrakenManager.cs
+++ Poseidon/Crypto/KrakenManager.cs
@@ -2,6 +2,22 @@ namespace Poseidon.Crypto
 {
     public class KrakenManager
     {
-        
+        private readonly int _rateLimitMilliseconds;
+        private readonly string _key;
+        private readonly string _signature;
+        private readonly string _url;
+        private readonly int _version;
+
+        /// <summary>
+        /// Constructor for Kraken
+        /// </summary>
+        /// <param name="rateLimitMilliseconds">Time to wait for polling data</param>
+        public KrakenManager(int rateLimitMilliseconds = 100)
+        {
+            _key = Settings.GetKraken_Key();
+            _signature = Settings.GetKraken_Signature();
+            _url = "https://api.kraken.com";
+            _version = 0;
+        }
     }
 }

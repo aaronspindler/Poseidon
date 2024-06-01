2ae4870f2a7ae1d1e97412efdaf0856a18a6979e
Aaron Spindler <aaron@xnovax.net>
2024-06-01 11:03:16 -0400

    Fix settings

--- config/settings.py
+++ config/settings.py
@@ -56,10 +56,10 @@ MIDDLEWARE = [
 ]
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#root-urlconf
-ROOT_URLCONF = "django_project.urls"
+ROOT_URLCONF = "config.urls"
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
-WSGI_APPLICATION = "django_project.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#templates
 TEMPLATES = [
